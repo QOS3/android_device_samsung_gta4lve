8b33753dc35fd05585e2a32045c147ac0c81314c
QOS3 <seefpro11@gmail.com>
2026-04-05 09:43:23 +0300

    fix shrp

--- shrp_gta4lve.mk
+++ shrp_gta4lve.mk
@@ -7,8 +7,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SHRP stuff.
-$(call inherit-product, vendor/shrp/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from gta4lve device
 $(call inherit-product, device/samsung/gta4lve/device.mk)
